00d724fe5570abec0f06e77ab6fb3d7b3cbe5892
webtunings <webtunings@gmail.com>
2015-04-19 21:06:55 +0530

    added comment

--- sorting/insertion_sort2.py
+++ sorting/insertion_sort2.py
@@ -1,11 +1,19 @@
 #insertion sort
+#sample - list
 def insertionSort(sample):
+#initial sample
  print("sample=",sample)
+
+#iterate over all elements of list
  for i in range(1,len(sample)):
+ #start comparing from the ith element
   j=i
+#go up to 2nd element
+#compare with previous element and swap it if required
   while (j!=0 and sample[j] < sample[j-1]):
    sample[j], sample[j-1] = sample[j-1], sample[j]
    j -= 1
+ #done
  print("sorted sample=",sample)
 
 sample1 = [1,2,3,4] 
