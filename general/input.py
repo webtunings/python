802297cb392873ff33782f5154528c1f8d3d6676
Praveen Kumar <webtunings@gmail.com>
2015-05-05 16:21:07 +0530

    input

---
+++ general/input.py
@@ -0,0 +1,12 @@
+>>> x = input("enter anything")
+enter anythinghello
+>>> x
+'hello'
+>>> x = int(input("enter number"))
+enter number45
+>>> x
+45
+>>> x = input("enter anything")
+enter anything45
+>>> x
+'45'
