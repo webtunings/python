4460193ec60f70dab8cdd87c6e0de92a4569733a
Praveen Kumar <webtunings@gmail.com>
2015-05-25 15:51:12 +0530

    in operator

---
+++ general/function/5_in_operator_list_String.py
@@ -0,0 +1,14 @@
+>>> test = [1,100,200,400]
+>>> 100 in test
+True
+>>> 22 in test
+False
+>>> test = "python language"
+>>> "python" in test
+True
+>>> "py" in test
+True
+>>> " python" in test
+False
+>>> "python " in test
+True
