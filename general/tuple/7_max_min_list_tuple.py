46f778450b92351ab78702c84853f1ee4a5dfd04
Praveen Kumar <webtunings@gmail.com>
2015-05-28 17:40:32 +0530

    min max list tuple

---
+++ general/tuple/7_max_min_list_tuple.py
@@ -0,0 +1,20 @@
+>>> t = (300,200,500)
+>>> max(t)
+500
+>>> min(t)
+200
+>>> t = ("python", "program", "tutorial")
+>>> max(t)
+'tutorial'
+>>> min(t)
+'program'
+>>> l = [300, 200, 500]
+>>> max(l)
+500
+>>> min(l)
+200
+>>> l = ["python", "program", "tutorial"]
+>>> max(l)
+'tutorial'
+>>> min(l)
+'program'
