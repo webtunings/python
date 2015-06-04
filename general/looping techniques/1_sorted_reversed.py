09073a9ccb41de9f5f6bb1ae88867fd6d40bacd9
Praveen Kumar <webtunings@gmail.com>
2015-06-04 15:04:22 +0530

    sorted reversed

---
+++ general/looping techniques/1_sorted_reversed.py
@@ -0,0 +1,58 @@
+>>> for i in range(10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> for i in reversed(range(10)):
+	print(i)
+
+	
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+>>> for i in sorted([2,5,1,3,4,9,6]):
+	print(i)
+
+	
+1
+2
+3
+4
+5
+6
+9
+>>> for i in sorted((3,2,1)):
+	print(i)
+
+	
+1
+2
+3
+>>> for i in sorted(set('zxdeacf')):
+	print(i)
+
+	
+a
+c
+d
+e
+f
+x
+z
+>>> 
