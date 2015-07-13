da08096e3d85556515f65b6b39d2acd95d559a2f
Praveen Kumar <webtunings@gmail.com>
2015-07-13 22:24:28 +0530

    string and float format

---
+++ general/formating/simple_format.py
@@ -0,0 +1,17 @@
+In [7]: import math
+
+In [8]: print("pi = %1.2f" % math.pi)
+pi = 3.14
+
+In [9]: print("pi = %1.3f" % math.pi)
+pi = 3.142
+
+In [10]: str1 = "first string"
+
+In [11]: str2 = "second string"
+
+In [12]: str3 = "%s is first one and %s is the second one" % (str1, str2) 
+
+In [13]: str3
+Out[13]: 'first string is first one and second string is the second one'
+
