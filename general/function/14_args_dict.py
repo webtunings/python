3ee6ce5d2474a0018bb7f014e47009a7e85c19ef
Praveen Kumar <webtunings@gmail.com>
2016-03-18 00:38:58 +0530

    variable argument - dictionary

---
+++ general/function/14_args_dict.py
@@ -0,0 +1,21 @@
+def test(**args):
+    print(args)
+    print(args["x"])
+    print(args["y"])
+    for key, value in args.items():
+        print("key = ", key)
+        print("value = ", value)
+    print(type(args))
+    
+test(x = 6, y = 10)
+
+'''
+{'y': 10, 'x': 6}
+6
+10
+key =  y
+value =  10
+key =  x
+value =  6
+<class 'dict'>
+'''
