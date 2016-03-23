d2bea66bbad9418aae0638673f98faddaa827363
Praveen Kumar <webtunings@gmail.com>
2016-03-23 16:29:19 +0530

    lookup

---
+++ general/class/4_inheritance_lookup.py
@@ -0,0 +1,18 @@
+class A:
+    def f(self):
+        return self.g()
+
+    def g(self):
+        return 'A'
+
+class B(A):
+    def g(self):
+        return 'B'
+
+a = A()
+b = B()
+print(a.f(), b.f())
+print(a.g(), b.g())
+
+#A B
+#A B
