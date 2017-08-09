0ee4f95cb2344251f0f41884a768e8934570df93
Praveen Kumar <webtunings@gmail.com>
2017-08-09 20:58:21 +0530

    code

--- general/re/finditer returns match object.py
+++ general/re/finditer returns match object.py
@@ -1,13 +1,12 @@
+import re
+pattern = '\w+'
+text = 'abc def'
 
-In [34]: pattern = '\w+'
+for match_object in re.finditer(pattern, text):
+    start = match_object.start()
+    end = match_object.end()
+    print('matched string = ', text[start:end], ' start = ', start, ' end = ', end)
 
-In [35]: text = 'abc def'
-
-In [38]: for match_object in re.finditer(pattern, text):
-   ....:     start = match_object.start()
-   ....:     end = match_object.end()
-   ....:     print('matched string = ', text[start:end], ' start = ', start, ' end = ', end)
-   ....:     
-matched string =  abc  start =  0  end =  3
-matched string =  def  start =  4  end =  7
+#matched string =  abc  start =  0  end =  3
+#matched string =  def  start =  4  end =  7
 
