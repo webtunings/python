0da06c8891f12b47e4c3d8f3b8b6b79def083476
Praveen Kumar <webtunings@gmail.com>
2017-08-09 21:01:20 +0530

    code

--- general/re/group and capture.py
+++ general/re/group and capture.py
@@ -1,10 +1,10 @@
->>> str = "email-new.user@gmail.com"
->>> match = re.search(r'([\w.-]+)@([\w.-]+)', str)
->>> match.group()
-'email-new.user@gmail.com'
->>> match.group(1)
-'email-new.user'
->>> match.group(2)
-'gmail.com'
->>> 
+import re
+str = "email-new.user@gmail.com"
+match = re.search(r'([\w.-]+)@([\w.-]+)', str)
+print(match.group())
+#'email-new.user@gmail.com'
+print(match.group(1))
+#'email-new.user'
+print(match.group(2))
+#'gmail.com'
 
