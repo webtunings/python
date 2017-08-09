bef16ea1a4a4fce7325127e418d2bc9709de539f
Praveen Kumar <webtunings@gmail.com>
2017-08-09 21:05:47 +0530

    code

--- general/re/match.start match.end match.re.pattern match.string.py
+++ general/re/match.start match.end match.re.pattern match.string.py
@@ -1,25 +1,22 @@
+import re
+pattern = "find me"
+text = "i am in between find me end"
+match = re.search(pattern, text)
+print(match.start())
+#16
 
-In [14]: pattern = "find me"
+print(match.end())
+#23
 
-In [15]: text = "i am in between find me end"
+print(match.re)
+#re.compile(r'find me', re.UNICODE)
 
-In [16]: match = re.search(pattern, text)
+print(match.re.pattern)
+#'find me'
 
-In [17]: match.start()
-Out[17]: 16
+print(match.string)
+#'i am in between find me end'
 
-In [18]: match.end()
-Out[18]: 23
-
-In [19]: match.re
-Out[19]: re.compile(r'find me', re.UNICODE)
-
-In [20]: match.re.pattern
-Out[20]: 'find me'
-
-In [22]: match.string
-Out[22]: 'i am in between find me end'
-
-In [23]: print("found %s in %s from %d to %d match is %s" % (match.re.pattern, match.string, match.start(), match.end(), text[match.start():match.end()]))
-found find me in i am in between find me end from 16 to 23 match is find me
+print("found %s in %s from %d to %d match is %s" % (match.re.pattern, match.string, match.start(), match.end(), text[match.start():match.end()]))
+#found find me in i am in between find me end from 16 to 23 match is find me
 
