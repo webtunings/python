c897718129297577178dd1452e15bb84213b6da2
Praveen Kumar <webtunings@gmail.com>
2017-08-09 21:15:01 +0530

    code

--- general/re/w and W.py
+++ general/re/w and W.py
@@ -1,24 +1,19 @@
->>> import re
->>> str = "test test123 test456"
->>> match = re.search(r'test\w\w\w', str)
->>> if match:
-...  print "matched"
-... else:
-...  print "not matched"
-... 
-matched
->>> match.group()
-'test123'
->>> match = re.search(r'test\W\W\W', str)
->>> if match:
-...  print "matched"
-... else:
-...  print "not matched"
-... 
-not matched
->>> match.group()
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-AttributeError: 'NoneType' object has no attribute 'group'
->>> 
+import re
+str = "test test123 test456"
+match = re.search(r'test\w\w\w', str)
+if match:
+  print("matched")
+else:
+  print("not matched")
+
+#matched
+print(match.group())
+#'test123'
+match = re.search(r'test\W\W\W', str)
+if match:
+  print("matched")
+else:
+  print("not matched")
+
+#not matched
 
