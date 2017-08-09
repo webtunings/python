d27ef7c78c68f3fcd5a615ecb928bc033d12e391
Praveen Kumar <webtunings@gmail.com>
2017-08-09 21:08:53 +0530

    code

--- general/re/one or more and character class.py
+++ general/re/one or more and character class.py
@@ -1,14 +1,15 @@
->>> str = "email-new.user@gmail.com"
->>> match = re.search(r'\w+@\w+', str)
->>> match
-<_sre.SRE_Match object at 0x2b3e8e400988>
->>> match.group()
-'user@gmail'
->>> match = re.search(r'[\w.-]@[\w.-]', str)
->>> match.group()
-'r@g'
->>> match = re.search(r'[\w.-]+@[\w.-]+', str)
->>> match.group()
-'email-new.user@gmail.com'
->>> 
+import re
+
+str = "email-new.user@gmail.com"
+match = re.search(r'\w+@\w+', str)
+print(match)
+#_sre.SRE_Match object at 0x2b3e8e400988>
+print(match.group())
+#'user@gmail'
+match = re.search(r'[\w.-]@[\w.-]', str)
+print(match.group())
+#'r@g'
+match = re.search(r'[\w.-]+@[\w.-]+', str)
+print(match.group())
+#'email-new.user@gmail.com'
 
