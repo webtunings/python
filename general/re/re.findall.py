1b630f9094bd4bf42da229b767c082b564adf552
Praveen Kumar <webtunings@gmail.com>
2017-08-09 21:10:24 +0530

    code

--- general/re/re.findall.py
+++ general/re/re.findall.py
@@ -1,9 +1,8 @@
-
->>> str = "new-email.1@cisco.com test.123.456-1@gmail.com"
->>> import re
->>> matches = re.findall(r'[\w.-]+@[\w.-]+', str)
->>> matches
-['new-email.1@cisco.com', 'test.123.456-1@gmail.com']
->>> 
+import re
+str = "new-email.1@cisco.com test.123.456-1@gmail.com"
+import re
+matches = re.findall(r'[\w.-]+@[\w.-]+', str)
+print(matches)
+#['new-email.1@cisco.com', 'test.123.456-1@gmail.com']
 
 
