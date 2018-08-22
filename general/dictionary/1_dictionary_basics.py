bd5377f3bf05a5ea8efb476737e78694554fa14b
Praveen <webtunings@gmail.com>
2018-08-22 14:16:03 +0530

    commit

--- general/dictionary/1_dictionary_basics.py
+++ general/dictionary/1_dictionary_basics.py
@@ -1,41 +1,60 @@
-d = {'x': [1,2,3], 'y': 123, 'z': "str"}
->>> type(d)
-<class 'dict'>
->>> d['x']
-[1, 2, 3]
->>> d['y']
-123
->>> d['a'] = 100
->>> d
-{'y': 123, 'a': 100, 'x': [1, 2, 3], 'z': 'str'}
->>> del d['a']
->>> d
-{'y': 123, 'x': [1, 2, 3], 'z': 'str'}
->>> d.keys()
-dict_keys(['y', 'x', 'z'])
->>> list(d.keys())
-['y', 'x', 'z']
->>> sorted(d.keys())
-['x', 'y', 'z']
->>> 'x' in d
-True
->>> 'a' in d
-False
->>> 'a' not in d
-True
->>> 'x' not in d
-False
->>> d = dict([('a', 100), ('b', 200)])
->>> d
-{'a': 100, 'b': 200}
->>> {x: x**3 for x in range(10)}
-{0: 0, 1: 1, 2: 8, 3: 27, 4: 64, 5: 125, 6: 216, 7: 343, 8: 512, 9: 729}
->>> {x: x**3 for x in (1,2,3)}
-{1: 1, 2: 8, 3: 27}
->>> {x: x**3 for x in [1,2,3]}
-{1: 1, 2: 8, 3: 27}
->>> dict(a=100,b=200)
-{'a': 100, 'b': 200}
->>> d.clear()
->>> d
-{}
+d = {'x': [1, 2, 3], 'y': 123, 'z': "str"}
+print(type(d))
+# <class 'dict'>
+
+print(d['x'])
+# [1, 2, 3]
+
+print(d['y'])
+# 123
+
+d['a'] = 100
+print(d)
+# {'y': 123, 'a': 100, 'x': [1, 2, 3], 'z': 'str'}
+
+del d['a']
+print(d)
+# {'y': 123, 'x': [1, 2, 3], 'z': 'str'}
+
+print(d.keys())
+# dict_keys(['x', 'y', 'z'])
+
+print(list(d.keys()))
+# ['x', 'y', 'z']
+
+print(sorted(d.keys()))
+# ['x', 'y', 'z']
+
+
+print('x' in d)
+# True
+
+print('a' in d)
+# False
+
+print('a' not in d)
+# True
+
+print('x' not in d)
+# False
+
+
+d = dict([('a', 100), ('b', 200)])
+print(d)
+# {'a': 100, 'b': 200}
+
+print({x: x**3 for x in range(10)})
+# {0: 0, 1: 1, 2: 8, 3: 27, 4: 64, 5: 125, 6: 216, 7: 343, 8: 512, 9: 729}
+
+print({x: x**3 for x in (1, 2, 3)})
+# {1: 1, 2: 8, 3: 27}
+
+print({x: x**3 for x in [1, 2, 3]})
+# {1: 1, 2: 8, 3: 27}
+
+print(dict(a=100, b=200))
+# {'a': 100, 'b': 200}
+
+d.clear()
+print(d)
+# {}
