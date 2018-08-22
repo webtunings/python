6e599d78c23411a903c6362273dfa6d57d76c7b2
Praveen <webtunings@gmail.com>
2018-08-22 15:58:29 +0530

    commit

--- general/function/6_default_args_mutable_nonMutable.py
+++ general/function/6_default_args_mutable_nonMutable.py
@@ -1,34 +1,49 @@
->>> x = 5
->>> def test(d = x):
-	print(d)
+x = 5
 
-	
->>> test()
-5
->>> test(10)
-10
->>> x = 40
->>> test()
-5
->>> def test(data, l = []):
-	l.append(data)
-	return l
 
->>> test(1)
-[1]
->>> test(2)
-[1, 2]
->>> def test(data, l = None):
-	if l is None:
-		l = []
-	l.append(data)
-	return l
+def test(d=x):
+    print(d)
 
->>> test(1)
-[1]
->>> test(2)
-[2]
->>> test(3,[200,300])
-[200, 300, 3]
->>> test(4)
-[4]
+
+test()
+# 5
+
+test(10)
+# 10
+
+x = 40
+test()
+# 5
+
+
+def test(data, l=[]):
+    l.append(data)
+    return l
+
+
+print(test(1))
+# [1]
+
+print(test(2))
+# [1, 2]
+
+
+def test(data, l=None):
+    if l is None:
+        l = []
+    l.append(data)
+    return l
+
+
+print(test(1))
+# [1]
+
+print(test(2))
+# [2]
+
+
+print(test(3,[200,300]))
+# [200, 300, 3]
+
+print(test(4))
+# [4]
