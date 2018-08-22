ba9fb3b6e30ad15d7dd28fa15ecd197abba6446a
Praveen <webtunings@gmail.com>
2018-08-22 15:38:00 +0530

    commit

--- general/function/5_in_operator_list_String.py
+++ general/function/5_in_operator_list_String.py
@@ -1,14 +1,14 @@
->>> test = [1,100,200,400]
->>> 100 in test
-True
->>> 22 in test
-False
->>> test = "python language"
->>> "python" in test
-True
->>> "py" in test
-True
->>> " python" in test
-False
->>> "python " in test
-True
+test = [1, 100, 200, 400]
+print(100 in test)
+# True
+print(22 in test)
+# False
+test = "python language"
+print("python" in test)
+# True
+print("py" in test)
+# True
+print(" python" in test)
+# False
+print("python " in test)
+# True
