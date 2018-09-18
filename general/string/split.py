9caf22cb15bffd73fe77384e27ca75aa8c8a4331
Praveen <webtunings@gmail.com>
2018-09-18 21:15:41 +0530

    commit

--- general/string/split.py
+++ general/string/split.py
@@ -3,3 +3,40 @@ print(str.split())
 #['a', 'bcd', 'ef', 'gh']
 str = "a,      bcd, ef, gh"
 print(str.split(","))
+
+str = "alpha "
+print(str.isalnum())
+#False
+
+str = "alpha"
+print(str.isalnum())
+#True
+
+str = "alpha123"
+print(str.isalnum())
+#True
+
+str = "alpha123"
+print(str.isalpha())
+#False
+
+str = "alpha"
+print(str.isalpha())
+#True
+
+
+str = "alpha"
+print(str.isdigit())
+#False
+
+str = "123"
+print(str.isdigit())
+#True
+
+str = "abc"
+print(str.islower())
+#True
+
+str = 'abc'
+print(str.isupper())
+#False
