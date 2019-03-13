9636c2c59e4224e324bf5cab1b1797a14607a1a0
Praveen Kumar <webtunings@gmail.com>
2019-03-13 23:15:56 +0530

    networking

--- networking/telnet.py
+++ networking/telnet.py
@@ -1,19 +1,25 @@
 import getpass
 import telnetlib
 
-HOST = "localhost"
+HOST = "100.1.1.3"
 user = input("Enter your remote account: ")
 password = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
-tn.read_until(b"login: ")
+tn.read_until(b"Username: ")
 tn.write(user.encode('ascii') + b"\n")
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"ls\n")
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"show ip ospf neighbor\n")
 tn.write(b"exit\n")
 
+
 print(tn.read_all().decode('ascii'))
+
+#gns3
+#cisco
